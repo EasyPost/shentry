51709ad4ad7c2ba8dec1bef2c63a83f0b21e89e9
James Brown <jbrown@easypost.com>
2017-12-13 18:18:49 -0800

    add python 3.6 to tests

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: POSIX",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: ISC License (ISCL)",
